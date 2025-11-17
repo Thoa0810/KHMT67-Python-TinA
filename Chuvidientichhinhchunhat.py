caa15a31e60e490fd3378a61ce74968136166b30
Thoa0810 <shucuteneeee2007@gmail.com>
2025-11-17 07:14:49 +0700

    chuvivadientichhcn

---
+++ Chuvidientichhinhchunhat.py
@@ -0,0 +1,8 @@
+chieu_dai = float(input("Nhập chiều dài: "))
+chieu_rong = float(input("Nhập chiều rộng: "))
+
+chu_vi = 2 * (chieu_dai + chieu_rong)
+dien_tich = chieu_dai * chieu_rong
+
+print("Chu vi hình chữ nhật là:", chu_vi)
+print("Diện tích hình chữ nhật là:", dien_tich)
