b875687fb44210528697a8c520f88858f8aceab9
Thoa0810 <shucuteneeee2007@gmail.com>
2025-11-17 07:12:08 +0700

    tinhtongvatich2so

---
+++ Tinhtong2sovatich2so.py
@@ -0,0 +1,8 @@
+a = int(input("Nhập số nguyên thứ nhất: "))
+b = int(input("Nhập số nguyên thứ hai: "))
+
+tong = a + b
+tich = a * b
+
+print("Tổng hai số là:", tong)
+print("Tích hai số là:", tich)
